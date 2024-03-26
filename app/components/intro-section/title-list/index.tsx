d3d19b9883b05e49cb855383bffb1aa39085a943
sayaneko514 <59753147+sayaneko514@users.noreply.github.com>
2024-03-26 03:39:06 -0700

    Remove unwanted shadow

--- app/components/intro-section/title-list/index.tsx
+++ app/components/intro-section/title-list/index.tsx
@@ -14,7 +14,7 @@ const TitleList: React.FC<TitleListProps> = ({ showTitle }) => {
             classNames="fade-up"
             unmountOnExit
         >
-            <p className="font-lora uppercase text-[0.5rem] sm:text-xs md:text-sm leading-9 tracking-widest pt-2 pb-6 text-white shadow-md" id="intro-title-list">
+            <p className="font-lora uppercase text-[0.5rem] sm:text-xs md:text-sm leading-9 tracking-widest pt-2 pb-6 text-white" id="intro-title-list">
                 <span className="inline-block before:content-['['] before:text-center before:inline-block before:px-2 before:text-white before:opacity-30 after:content-[']'] after:text-center after:inline-block after:pl-2 after:pr-3 after:text-white after:opacity-30" id="intro-title-list-text">
                     <ReactTyped
                         strings={["Software Developer", "Web Developer", "Software Developer in Test"]}
