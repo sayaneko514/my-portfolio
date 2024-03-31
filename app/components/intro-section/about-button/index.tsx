fb2831ac1d2ea83894bd6b8b253f282bd887af63
sayaneko514 <59753147+sayaneko514@users.noreply.github.com>
2024-03-31 02:50:59 -0700

    Update #8 March 31st
    • Implement smooth scroll for the about button

--- app/components/intro-section/about-button/index.tsx
+++ app/components/intro-section/about-button/index.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react';
 import { CSSTransition } from 'react-transition-group';
 
@@ -6,6 +7,19 @@ interface AboutMeButtonProps {
 }
 
 const AboutMeButton: React.FC<AboutMeButtonProps> = ({ showButton }) => {
+
+    const handleClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
+        e.preventDefault();
+        const aboutSection = document.querySelector('#about-section');
+
+        if (aboutSection) {
+            window.scrollTo({
+                top: aboutSection.getBoundingClientRect().top + window.scrollY,
+                behavior: 'smooth',
+            });
+        }
+    };
+
     return (
         <CSSTransition
             in={showButton}
@@ -13,13 +27,14 @@ const AboutMeButton: React.FC<AboutMeButtonProps> = ({ showButton }) => {
             classNames="fade-up"
             unmountOnExit
         >
-            <a
+            <Link
                 href="#about-section"
+                onClick={handleClick}
                 id="about-button"
                 className="inline-block text-white border border-white-30 hover:border-highlight focus:border-highlight hover:text-highlight hover:bg-black hover:bg-opacity-20 px-7 py-0.5 text-sm sm:text-base md:text-lg uppercase tracking-widest transition-colors duration-300 ease-in-out"
             >
                 About Me
-            </a>
+            </Link>
         </CSSTransition>
     );
 };
