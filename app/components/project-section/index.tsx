c2c074c82884ec7a272da86088d3b67c8c03d12f
sayaneko514 <59753147+sayaneko514@users.noreply.github.com>
2024-04-06 14:59:39 -0700

    0.5.2.1 Update
    • Fixed file name cases

--- app/components/project-section/index.tsx
+++ app/components/project-section/index.tsx
@@ -8,15 +8,15 @@ const Project = () => {
         {
             title: "Protfolio Page",
             description: "A sleek personal portfolio page built with Next.js, featuring React, TypeScript, JavaScript, and Tailwind CSS for responsive design, with MongoDB as the backend. Hosted on Vercel for seamless deployment and performance.",
-            imageStatic: '/images/portfolio.png',
-            imageGif: '/images/portfolio.gif',
+            imageStatic: '/images/Portfolio.png',
+            imageGif: '/images/Portfolio.gif',
             elements: ['React', 'Typescript', 'Javascript', 'Tailwind', 'Mongodb', 'Vercel']
         },
         {
             title: "Barbie Gym",
             description: "Barbie Gym's landing page is the start of an expansive project, poised to evolve into a comprehensive login portal for a fitness center in Japan, blending T3 tech with intuitive design.",
-            imageStatic: '/images/barbie.png',
-            imageGif: '/images/barbie.gif',
+            imageStatic: '/images/Barbie.png',
+            imageGif: '/images/Barbie.gif',
             elements: ['React', 'Typescript', 'Javascript', 'Tailwind', 'Mysql', 'Prisma', 'Vercel']
         },
     ];
